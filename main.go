eb5c5bf0ab7573bc721afb5b384fbe138a85a9ae
Akhil Krishna A <akhilkrishna@locus.sh>
2025-01-29 15:09:26 +0530

    cscs

--- main.go
+++ main.go
@@ -71,8 +71,8 @@ type RequestData struct {
 	Path string `parquet:"name=path, type=UTF8"`
 	Host string `parquet:"name=host, type=UTF8"`
 	// Headers map[string]string `parquet:"name=headers, type=MAP`
-	IP   string `parquet:"name=ip, type=UTF8"`
-	Body string `parquet:"name=body, type=UTF8"`
+	IP string `parquet:"name=ip, type=UTF8"`
+	// Body string `parquet:"name=body, type=UTF8"`
 }
 
 func (h *httpStreamFactory) New(net, transport gopacket.Flow) tcpassembly.Stream {
@@ -157,8 +157,8 @@ func (basics BucketBasics) forwardRequest(req *http.Request, reqSourceIP string,
 		Path: req.URL.Path,
 		Host: req.Host,
 		// Headers: headersMap,
-		IP:   reqSourceIP,
-		Body: string(body),
+		IP: reqSourceIP,
+		// Body: string(body),
 	}
 
 	parquetBuffer, err := convertToParquet(requestData)
